20e2e31095dafdf3a48e6fdccbee98b6d45581d2
Liukin <contacto@pc-solucion.es>
2025-03-07 11:20:15 +0100

    Premios en json

---
+++ items.js
@@ -0,0 +1,68 @@
+const ruletaItems = [
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700984860/Aeternum/esmeralda_tallada_impecable.png",
+        title: "x20 Esmeralda Talladas Impecables",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700984703/Aeternum/piedra_de_luna_tallada_impecable.png",
+        title: "x20 Piedra de Luna Talladas Impecables",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700982271/Aeternum/rubi_tallado_impecable.png",
+        title: "x20 Rubí Tallados Impecables",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700979814/Aeternum/ambar_tallado_impecable.png",
+        title: "x20 Ámbar Tallado Impecable",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700453352/Aeternum/protecci%C3%B3n_fisica_4.png",
+        title: "x20 Ónice Tallado Impecable",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700416662/Aeternum/opalcutt4.png",
+        title: "x20 Ópalo Tallado Impecable",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1699795933/Aeternum/cornalina_tallada_impecable.png",
+        title: "x20 Cornalina Tallada Impecable",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1699103941/Aeternum/amatista.png",
+        title: "x20 Amatista Tallada Impecable",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1699102512/Aeternum/diamante.png",
+        title: "x20 Dimante Tallado Impecable",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1682325800/coin_stack_5000.webp",
+        title: "x5.000 Gold",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1682325800/coin_stack_5000.webp",
+        title: "x10.000 Gold",
+        subtitle: ""
+    }
+]; 
