02142994cb08b3535917a234396a0b2e972607f5
Liukin <contacto@pc-solucion.es>
2025-03-08 08:08:29 +0100

    Nuevos fondos y recompensas

--- items.js
+++ items.js
@@ -1,68 +1,199 @@
 const ruletaItems = [
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700984860/Aeternum/esmeralda_tallada_impecable.png",
         title: "x20 Esmeralda Talladas Impecables",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/ingott53.png",
+        title: "x10 Lingote Prismático",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700984703/Aeternum/piedra_de_luna_tallada_impecable.png",
         title: "x20 Piedra de Luna Talladas Impecables",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700982271/Aeternum/rubi_tallado_impecable.png",
         title: "x20 Rubí Tallados Impecables",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/leathert53.png",
+        title: "x10 Cuero Prismático",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700979814/Aeternum/ambar_tallado_impecable.png",
         title: "x20 Ámbar Tallado Impecable",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
-        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700453352/Aeternum/protecci%C3%B3n_fisica_4.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/clotht53.png",
+        title: "x10 Tela Prismática",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700453352/Aeternum/protección_fisica_4.png",
         title: "x20 Ónice Tallado Impecable",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/resource/matrix-jewelryt52.png",
+        title: "x1 Matriz de Joyería",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1700416662/Aeternum/opalcutt4.png",
         title: "x20 Ópalo Tallado Impecable",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/blockt53.png",
+        title: "x10 Bloque Prismático",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1699795933/Aeternum/cornalina_tallada_impecable.png",
         title: "x20 Cornalina Tallada Impecable",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/fooddext51.png",
+        title: "x100 Natillas de Plátano",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/resource/matrix-armort52.png",
+        title: "x1 Matriz de Armadura",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1699103941/Aeternum/amatista.png",
         title: "x20 Amatista Tallada Impecable",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/timbert53.png",
+        title: "x10 Tablones Prismáticos",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1699102512/Aeternum/diamante.png",
         title: "x20 Dimante Tallado Impecable",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/foodintt51.png",
+        title: "x100 Macedonia de Frutas",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/bg-gold.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1682325800/coin_stack_5000.webp",
         title: "x5.000 Gold",
         subtitle: ""
     },
     {
-        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1.png",
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/fooddext5.png",
+        title: "x50 Albenaja Frita",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/resource/prismaticscarab.png",
+        title: "x1 Escarabajo Prismático",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/bg-gold.png",
         image: "https://res.cloudinary.com/pcsolucion/image/upload/v1682325800/coin_stack_5000.webp",
         title: "x10.000 Gold",
         subtitle: ""
-    }
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/honingstonet5.png",
+        title: "x20 Piedras de Afilar potentes",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/resource/goldenscarab.png",
+        title: "x2 Escarabajos de Oro",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/resource/matrix-weapont52.png",
+        title: "x1 Matriz de Arma",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/resource/goldenscarab.png",
+        title: "x4 Escarabajos de Oro",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://pc-solucion.es/wp-content/uploads/2025/03/foodcont51.png",
+        title: "x100 Parfait de Plátano",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/restorationserumt5.png",
+        title: "x100 Sueros de Regeneración",
+        subtitle: ""
+    },{
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/gemstonedustt5.png",
+        title: "x100 Polvo de Gemas Potente",
+        subtitle: ""
+    },
+    ,{
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/foodstrt5.png",
+        title: "x50 Pez Ñú Asado",
+        subtitle: ""
+    },
+    ,{
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/foodfoct5.png",
+        title: "x50 Carpa de Lava",
+        subtitle: ""
+    },
+    ,{
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/fooddext5.png",
+        title: "x50 Albenaja Frita",
+        subtitle: ""
+    },
+    {
+        background: "https://pc-solucion.es/wp-content/uploads/2025/03/consumergrade-1-1.png",
+        image: "https://cdn.nwdb.info/db/images/live/v52/icons/items/consumable/incenset5.png",
+        title: "x100 Incienso Potente",
+        subtitle: ""
+    },
 ]; 
--- script.js
+++ script.js
@@ -1,19 +1,42 @@
+function generateItems() {
+    const wrapper = $(".roulette-item-wrapper");
+    
+    // Repetir los items varias veces para crear el efecto de ruleta infinita
+    const repetitions = 10;
+    for (let i = 0; i < repetitions; i++) {
+        ruletaItems.forEach(item => {
+            const itemHtml = `
+                <div class="roulette-item">
+                    <div class="img-back">
+                        <img src="${item.background}" alt="itemback" class="roulette-item-back">
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="${item.image}" alt="GameItem">
+                    </div>
+                    <h3 class="roulette-subtitle-first">${item.title}</h3>
+                    <h3 class="roulette-subtitle-second">${item.subtitle}</h3>
+                </div>
+            `;
+            wrapper.append(itemHtml);
+        });
+    }
+}
+
 function slideImages() {
-  var widthHalfContainer = $("body > .container").outerWidth() / 2;
-  var parent = $(".roulette-item-wrapper");
-  var totalItems = $(".roulette-item-wrapper").children().length;
-  var randomValue = Math.floor(Math.random() * (totalItems * 4)) + 1; // Genera un valor aleatorio entre 1 y el doble del total de elementos
-  
-  // Asegurarse de que randomValue esté dentro del rango de índices válidos
-  var actualIndex = (randomValue - 1) % totalItems;
-  
-  var spunAmount = $(".roulette-item-wrapper").children()[actualIndex].getBoundingClientRect().left - 626.8333129882812 + 138/2;
-  
-  console.log(widthHalfContainer, randomValue, spunAmount);
-  
-  $(".roulette-item").css("transform", "translateX(-" + spunAmount + "px)");
+    var widthHalfContainer = $("body > .container").outerWidth() / 2;
+    var parent = $(".roulette-item-wrapper");
+    var totalItems = $(".roulette-item-wrapper").children().length;
+    var randomValue = Math.floor(Math.random() * (totalItems * 4)) + 1;
+    
+    var actualIndex = (randomValue - 1) % totalItems;
+    var spunAmount = $(".roulette-item-wrapper").children()[actualIndex].getBoundingClientRect().left - 626.8333129882812 + 138/2;
+    
+    console.log(widthHalfContainer, randomValue, spunAmount);
+    
+    $(".roulette-item").css("transform", "translateX(-" + spunAmount + "px)");
 }
 
 $(document).ready(function() {
-  slideImages();
+    generateItems();
+    slideImages();
 });
