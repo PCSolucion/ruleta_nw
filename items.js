10092c9cc0d8ef153331478462fb42a9892d0f3d
Liukin <contacto@pc-solucion.es>
2025-05-15 03:03:48 +0200

    premios actualizados mayo 2025

--- items.js
+++ items.js
@@ -9,31 +9,31 @@ const ruletaItems = [
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1700984860/Aeternum/esmeralda_tallada_impecable.png`,
-        title: "x20 Esmeralda Talladas Impecables",
+        title: "x10 Esmeralda Talladas Impecables",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1700984703/Aeternum/piedra_de_luna_tallada_impecable.png`,
-        title: "x20 Piedra de Luna Talladas Impecables",
+        title: "x10 Piedra de Luna Talladas Impecables",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1700982271/Aeternum/rubi_tallado_impecable.png`,
-        title: "x20 Rubí Tallados Impecables",
+        title: "x10 Rubí Tallados Impecables",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1700979814/Aeternum/ambar_tallado_impecable.png`,
-        title: "x20 Ámbar Tallado Impecable",
+        title: "x10 Ámbar Tallado Impecable",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1700453352/Aeternum/protección_fisica_4.png`,
-        title: "x20 Ónice Tallado Impecable",
+        title: "x10 Ónice Tallado Impecable",
         subtitle: ""
     },
     {
@@ -45,25 +45,25 @@ const ruletaItems = [
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1700416662/Aeternum/opalcutt4.png`,
-        title: "x20 Ópalo Tallado Impecable",
+        title: "x10 Ópalo Tallado Impecable",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `https://pc-solucion.es/wp-content/uploads/2025/03/blockt53.png`,
-        title: "x10 Bloque Prismático",
+        title: "x3 Bloque Prismático",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1699795933/Aeternum/cornalina_tallada_impecable.png`,
-        title: "x20 Cornalina Tallada Impecable",
+        title: "x10 Cornalina Tallada Impecable",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `https://pc-solucion.es/wp-content/uploads/2025/03/fooddext51.png`,
-        title: "x100 Natillas de Plátano",
+        title: "x20 Natillas de Plátano",
         subtitle: ""
     },
     {
@@ -75,37 +75,37 @@ const ruletaItems = [
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1699103941/Aeternum/amatista.png`,
-        title: "x20 Amatista Tallada Impecable",
+        title: "x10 Amatista Tallada Impecable",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `https://pc-solucion.es/wp-content/uploads/2025/03/timbert53.png`,
-        title: "x10 Tablones Prismáticos",
+        title: "x4 Tablones Prismáticos",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${CLOUDINARY_URL}v1699102512/Aeternum/diamante.png`,
-        title: "x20 Dimante Tallado Impecable",
+        title: "x10 Dimante Tallado Impecable",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `https://pc-solucion.es/wp-content/uploads/2025/03/foodintt51.png`,
-        title: "x100 Macedonia de Frutas",
+        title: "x20 Macedonia de Frutas",
         subtitle: ""
     },
     {
         background: GOLD_BACKGROUND,
         image: `${CLOUDINARY_URL}v1682325800/coin_stack_5000.webp`,
-        title: "x5.000 Gold",
+        title: "x1.000 Gold",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/fooddext5.png`,
-        title: "x20 Albenaja Frita",
+        title: "x10 Albenaja Frita",
         subtitle: ""
     },
     {
@@ -117,19 +117,19 @@ const ruletaItems = [
     {
         background: GOLD_BACKGROUND,
         image: `${CLOUDINARY_URL}v1682325800/coin_stack_5000.webp`,
-        title: "x10.000 Gold",
+        title: "x1.200 Gold",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/honingstonet5.png`,
-        title: "x20 Piedras de Afilar potentes",
+        title: "x10 Piedras de Afilar potentes",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}resource/goldenscarab.png`,
-        title: "x2 Escarabajos de Oro",
+        title: "x1 Escarabajo de Oro",
         subtitle: ""
     },
     {
@@ -141,49 +141,49 @@ const ruletaItems = [
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}resource/goldenscarab.png`,
-        title: "x4 Escarabajos de Oro",
+        title: "x1 Escarabajos de Oro",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `https://pc-solucion.es/wp-content/uploads/2025/03/foodcont51.png`,
-        title: "x100 Parfait de Plátano",
+        title: "x20 Parfait de Plátano",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/restorationserumt5.png`,
-        title: "x100 Sueros de Regeneración",
+        title: "x20 Sueros de Regeneración",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/gemstonedustt5.png`,
-        title: "x100 Polvo de Gemas Potente",
+        title: "x20 Polvo de Gemas Potente",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/foodstrt5.png`,
-        title: "x50 Pez Ñú Asado",
+        title: "x15 Pez Ñú Asado",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/foodfoct5.png`,
-        title: "x50 Carpa de Lava",
+        title: "x20 Carpa de Lava",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/fooddext5.png`,
-        title: "x15 Albenaja Frita",
+        title: "x10 Albenaja Frita",
         subtitle: ""
     },
     {
         background: DEFAULT_BACKGROUND,
         image: `${NWDB_URL}consumable/incenset5.png`,
-        title: "x100 Incienso Potente",
+        title: "x20 Incienso Potente",
         subtitle: ""
     },
 ]; 
